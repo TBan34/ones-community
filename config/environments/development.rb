28c81ee9595bdd150deaf7bfe90401138a30a59f
TBan34 <tb3358@gmail.com>
2023-02-06 06:05:35 +0000

    [Add]deviseのインストール、adminモデルの作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,9 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   
+  # Railsアプリケーションを画面表示するため
+  config.hosts.clear
+  
   # ActiveStorageによるエラー回避のため
   config.active_job.queue_adapter = :inline
 end
