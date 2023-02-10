7346671798d8f41de09fb83d2868fa4c69d60d62
TBan34 <tb3358@gmail.com>
2023-02-10 00:58:23 +0000

    [Add]管理者側にユーザー一覧・詳細機能、投稿一覧・検索機能を実装

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,25 @@
 class Admin::PostsController < ApplicationController
+  
+  def index
+    @posts = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts : Post.all
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:user_id, :title, :body, :time, :place, :belonging, :image, tag_ids: [])
+  end
+  
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,25 @@
 class Admin::UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :prefecture, :municipality, :telephone_number, :display_name, :self_introduction, :profile_image)
+  end
+    
 end
