b1bd0654bf2eadbe87f7b917e124fed9e55faaf1
TBan34 <tb3358@gmail.com>
2023-03-22 11:30:57 +0000

    [Update]不要な外部キーを削除、管理者アドレスの変更

--- db/migrate/20230206075153_create_posts.rb
+++ db/migrate/20230206075153_create_posts.rb
@@ -2,7 +2,6 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.integer "user_id", null: false
-      t.integer "room_id", null: true
       t.string "title", null: false
       t.text "body", null: false
       t.datetime "since_when", null: false
