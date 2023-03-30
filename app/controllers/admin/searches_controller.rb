7659aa6a8dc7440640e7719b09803cc4f6db1d45
TBan34 <tb3358@gmail.com>
2023-03-30 05:23:28 +0000

    [Update]ルーム、チャット、コメントのビューファイルの調整

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -28,12 +28,12 @@ class Admin::SearchesController < ApplicationController
       # 検索にヒットした全ユーザーのID、及び関連する投稿のIDを取得し、ルームを絞り込み
       user_ids = User.where("display_name LIKE ?", "%#{@search_room}%").pluck(:id)
       post_ids = Post.where(user_id: user_ids).pluck(:id)
-      @rooms = Room.where(post_id: post_ids).page(params[:page])
+      @rooms = Room.where(post_id: post_ids).order(created_at: :desc).page(params[:page])
       render "admin/searches/room_result"
     else
       # 検索にヒットした全投稿のIDから、ルームを絞り込み
       post_ids = Post.where("title LIKE ?", "%#{@search_room}%").pluck(:id)
-      @rooms = Room.where(post_id: post_ids).page(params[:page])
+      @rooms = Room.where(post_id: post_ids).order(created_at: :desc).page(params[:page])
       render "admin/searches/room_result"
     end
   end
